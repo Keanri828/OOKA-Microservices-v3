0a620046be8875eae98b104b31ac694e5fb51f97
Keanri828 <57674945+Keanri828@users.noreply.github.com>
2021-03-16 20:55:41 +0100

    Angular Komponenten + Service für Requests

---
+++ ms-frontend-app/src/app/analysis-history/analysis-history.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AnalysisHistoryComponent } from './analysis-history.component';
+
+describe('AnalysisHistoryComponent', () => {
+  let component: AnalysisHistoryComponent;
+  let fixture: ComponentFixture<AnalysisHistoryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AnalysisHistoryComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AnalysisHistoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ms-frontend-app/src/app/analysis-history/analysis-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analysis-history',
+  templateUrl: './analysis-history.component.html',
+  styleUrls: ['./analysis-history.component.css']
+})
+export class AnalysisHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
