8fcb8d3dd84ecc517089a9bf1b3e892efa8c7343
Robin0815 <robin_frank_ccaa@yahoo.de>
2021-03-16 17:08:49 +0100

    analyse service 2 added

---
+++ analyse-service2/src/main/java/keanri828/ookamicroservices/analyseservice2/AnalyseService2Application.java
@@ -0,0 +1,13 @@
+package keanri828.ookamicroservices.analyseservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyseService2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyseService2Application.class, args);
+    }
+
+}
---
+++ analyse-service2/src/test/java/keanri828/ookamicroservices/analyseservice2/AnalyseService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package keanri828.ookamicroservices.analyseservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyseService2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
