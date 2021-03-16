33a9fc29e6b793c640558a2972ab74c2ba4de6c7
Keanri828 <57674945+Keanri828@users.noreply.github.com>
2021-03-16 04:48:23 +0100

    persistency service init

---
+++ persistency-service/src/main/java/keanri828/ookamicroservices/persistencyservice/PersistencyServiceApplication.java
@@ -0,0 +1,13 @@
+package keanri828.ookamicroservices.persistencyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistencyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistencyServiceApplication.class, args);
+    }
+
+}
