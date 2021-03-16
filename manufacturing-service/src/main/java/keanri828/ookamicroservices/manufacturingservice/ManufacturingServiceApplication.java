63d7a7fce7ac7ccb3c1a032c521b3943982d05e7
Keanri828 <57674945+Keanri828@users.noreply.github.com>
2021-03-16 18:16:05 +0100

    manufacturing service init

---
+++ manufacturing-service/src/main/java/keanri828/ookamicroservices/manufacturingservice/ManufacturingServiceApplication.java
@@ -0,0 +1,13 @@
+package keanri828.ookamicroservices.manufacturingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManufacturingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManufacturingServiceApplication.class, args);
+    }
+
+}
---
+++ manufacturing-service/src/test/java/keanri828/ookamicroservices/manufacturingservice/ManufacturingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package keanri828.ookamicroservices.manufacturingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManufacturingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
