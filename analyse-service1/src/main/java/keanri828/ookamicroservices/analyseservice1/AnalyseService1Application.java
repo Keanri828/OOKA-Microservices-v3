5f7e2ef22fbbc0c157297e58207bdeadeb6bcc1c
Fiona Brücher <fiona.bruecher@smail.inf.h-brs.de>
2021-03-16 17:30:04 +0100

    init Analyse-Service 1

---
+++ analyse-service1/src/main/java/keanri828/ookamicroservices/analyseservice1/AnalyseService1Application.java
@@ -0,0 +1,13 @@
+package keanri828.ookamicroservices.analyseservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyseService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyseService1Application.class, args);
+    }
+
+}
---
+++ analyse-service1/src/test/java/keanri828/ookamicroservices/analyseservice1/AnalyseService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package keanri828.ookamicroservices.analyseservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyseService1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
