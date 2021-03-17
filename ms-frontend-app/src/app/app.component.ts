9a98b8d7298e09379b469d5105b6dcf9203afe50
Keanri828 <57674945+Keanri828@users.noreply.github.com>
2021-03-17 11:20:33 +0100

    Trennung von Konfiguration und Historie. Ändert sich vielleicht noch

--- ms-frontend-app/src/app/app.component.ts
+++ ms-frontend-app/src/app/app.component.ts
@@ -7,5 +7,5 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Wir Schiffen Das';
+  title = 'Wir Schiffen Das: Motor-Konfiguration';
 }
