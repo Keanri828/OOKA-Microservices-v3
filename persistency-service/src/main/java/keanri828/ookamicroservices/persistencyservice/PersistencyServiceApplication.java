3458cbede0cee6c776dcc78cf2433f7908fd88c3
Robin0815 <robin_frank_ccaa@yahoo.de>
2021-03-18 14:30:13 +0100

    Database now running persistent, finally, db object in C /data/demo is nit overwritten anymore

--- persistency-service/src/main/java/keanri828/ookamicroservices/persistencyservice/PersistencyServiceApplication.java
+++ persistency-service/src/main/java/keanri828/ookamicroservices/persistencyservice/PersistencyServiceApplication.java
@@ -2,6 +2,7 @@ package keanri828.ookamicroservices.persistencyservice;
 
 import com.netflix.discovery.EurekaClient;
 import keanri828.ookamicroservices.persistencyservice.databaseconf.ConfigService;
+import keanri828.ookamicroservices.persistencyservice.databaseconf.PersitencyFeign;
 import keanri828.ookamicroservices.persistencyservice.entities.ConfigEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -10,11 +11,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -30,13 +32,41 @@ public class PersistencyServiceApplication {
     @Autowired
     ConfigService configService;
 
+    @Autowired
+    PersitencyFeign persitencyFeign;
+
     @PostMapping(
-            value = "/save",
+            value = "/config/save",
             consumes = "application/json"
     )
     public void analyseConfig(@RequestBody ConfigEntity dto){
 
         configService.save(dto);
+    }
+
+    @GetMapping(
+            value = "/config/get/{id}",
+            produces = "application/json"
+    )
+    public ConfigEntity getConfigById(@PathVariable UUID id){
+        return configService.findById(id);
+    }
+
+
+    @GetMapping(
+            value = "/config/all",
+            produces = "application/json"
+    )
+    public List<ConfigEntity> getAllConfig(){
+        return configService.findAll();
+    }
+
+    @DeleteMapping(
+            value = "/config/{id}"
+    )
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCondigById(@PathVariable UUID id) {
+        configService.deleteById(id);
     }
 
 }
--- persistency-service/src/main/java/keanri828/ookamicroservices/persistencyservice/databaseconf/ConfigService.java
+++ persistency-service/src/main/java/keanri828/ookamicroservices/persistencyservice/databaseconf/ConfigService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ConfigService {
@@ -27,6 +29,14 @@ public class ConfigService {
 
     public void save(ConfigEntity configEntity) {
         configRepository.save(configEntity);
-
+    }
+    public void deleteById(UUID id) {
+        configRepository.deleteById(id);
+    }
+    public void deleteAll(){
+        configRepository.deleteAll();
+    }
+    public ConfigEntity findById(UUID id){
+        return configRepository.findById(id).orElse(null);
     }
 }
